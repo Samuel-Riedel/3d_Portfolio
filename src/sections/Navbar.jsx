6fe34d048fb92d4aa46e13970ec32d850d25e754
Samuel Riedel <samuel.riedel@outlook.com>
2025-07-23 00:56:40 +0000

    test_text_for_netlify

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -4,8 +4,8 @@ import {motion} from "motion/react"
 function Navigation(){
     return <ul className="nav-ul">
         <li className="nav-li"><a href="" className="nav-link">Home</a></li>
-        <li className="nav-li"><a href="" className="nav-link">Home</a></li>
-        <li className="nav-li"><a href="" className="nav-link">Home</a></li>
+        <li className="nav-li"><a href="" className="nav-link">Store</a></li>
+        <li className="nav-li"><a href="" className="nav-link">Github</a></li>
         </ul>
 }
 const Navbar = () => {
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className="fixed insert-x-0 z-20 w-full backdrop-blur-lg bg-primary/40">Navbar
         <div className="mx-auto c-space max-w-7xl">
             <div className="flex items-center justify-between py-2 sm:py-0">
-                <a href="/" className="text-xl font-bold transition-colors text-neutral-400 hover:text-white">Trabalha Lucas y Alan</a>
+                <a href="/" className="text-xl font-bold transition-colors text-neutral-400 hover:text-white">Trabalhem Lucas y Alan dejen de falar tanto</a>
                 <button onClick={()=> setIsOpen(!isOpen)} className="flex cursor-pointer text-neutral-400 hover:text-white focus: outline-none sm:hidden"> <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} className="w-6 h-6"></img></button>
                 <nav className="hidden sm:flex"><Navigation /></nav>
             </div>
