85a703340c1df2ff9fc070bfb2bdf1158f91c1ce
Samuel Riedel <samuel.riedel@outlook.com>
2025-07-23 23:36:17 +0000

    finished_mobile_version_for_motion_plugin

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,5 +1,7 @@
 import { FlipWords } from "./FlipWords";
 
+const words= ["Secure", "Modern", "Scalable"];
+
 const HeroText = () => {
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3x bg-clip-text">
@@ -14,12 +16,21 @@ const HeroText = () => {
           {/*<div>This is just Placeholder Text</div>*/}
         </div>{" "}
         <FlipWords
-          words={["Secure", "Modern", "Scalable"]}
+          words={words}
           className="font-black text-white text-8xl"
         />
         <p className="text-4xl font-medium text-neutral-300">Web Solutions</p>
       </div>
       {/*Mobile View*/}
+      <div className="flex flex-col space-y6 md:hidden">
+        {" "}
+        <p className="text-4xl font-medium"> Hi, Im Samuel</p>{" "}
+        <div>
+          <p className="text-5xl font-black text-neutral-100"> Building </p>
+          <div> <FlipWords words={words} className="font-bold text-white text-7xl"/> </div>
+          <p className="text-4xl font-black text-neutral-300">Web Applications</p>
+        </div>
+      </div>
     </div>
   );
 };
