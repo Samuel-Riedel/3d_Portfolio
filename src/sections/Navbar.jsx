b91fd72b53b04250318e8ba582c23a08f30724c3
Samuel Riedel <samuel.riedel@outlook.com>
2025-07-23 00:17:51 +0000

    navbar_for_desktop_mobile_version

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
+function Navigation(){
+    return <ul className="nav-ul">
+        <li className="nav-li"><a href="" className="nav-link">Home</a></li>
+        <li className="nav-li"><a href="" className="nav-link">Home</a></li>
+        <li className="nav-li"><a href="" className="nav-link">Home</a></li>
+        </ul>
+}
 const Navbar = () => {
+    const[isOpen,setIsOpen] = useState(false)
   return (
-    <div>Navbar</div>
+    <div className="fixed insert-x-0 z-20 w-full backdrop-blur-lg bg-primary/40">Navbar
+        <div className="mx-auto c-space max-w-7xl">
+            <div className="flex items-center justify-between py-2 sm:py-0">
+                <a href="/" className="text-xl font-bold transition-colors text-neutral-400 hover:text-white">Trabalha Lucas y Alan</a>
+                <button onClick={()=> setIsOpen(!isOpen)} className="flex cursor-pointer text-neutral-400 hover:text-white focus: outline-none sm:hidden"> <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} className="w-6 h-6"></img></button>
+                <nav className="hidden sm:flex"><Navigation /></nav>
+            </div>
+         </div>
+         {isOpen &&(
+            <div className="block overflow-hidden text-center sm:hidden">
+                <nav className="pb-5"><Navigation />
+                </nav>
+                </div>)}
+    </div>
   );
 };
 
-export default App;
+export default Navbar;
