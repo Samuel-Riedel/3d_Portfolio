92037df78e44c70e9ded1517dc2d81d9d7a61d50
Samuel Riedel <samuel.riedel@outlook.com>
2025-07-25 01:20:48 +0000

    added_images_background_for_parallax_effect

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./sections/Hero";
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
-      Web Version 0.0.0.1-J
+      Web Version 0.0.2.1-J
       <Navbar />
       <Hero />
     </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,12 @@
 import HeroText from "../components/HeroText";
+import ParallaxBackground from "../components/ParallaxBackground";
 
 export const Hero = () => {
   return (
     <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
       Hero
       <HeroText />
+      <ParallaxBackground />
     </section>
   );
 };
