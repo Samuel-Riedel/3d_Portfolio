c6fd9b0beb7e00a4e0f8167debbfe54fec2889ed
Samuel Riedel <samuel.riedel@outlook.com>
2025-07-26 00:26:45 +0000

    added_loader_component__for_Astronaut_jsx_object

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,8 +5,12 @@ import { Canvas } from "@react-three/fiber";
 import { Astronaut } from "../components/Astronaut";
 import HeroText from "../components/HeroText";
 import ParallaxBackground from "../components/ParallaxBackground";
-import { OrbitControls } from "@react-three/drei";
+import { Float, OrbitControls } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
+import { easing } from "maath";
+import { useFrame } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
 
 export const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 853 });
@@ -20,16 +24,32 @@ export const Hero = () => {
         style={{ width: "100vw", height: "100vh" }}
       >
         <Canvas camera={{ position: [0, 1, 3] }}>
-          <Astronaut
-            scale={isMobile && 0.23}
-            position={isMobile && [0, -1.5, 0]}
-          />
-          <OrbitControls />
-          {/*{enableZoom={false}*/}
+          <Suspense fallback={<Loader />}>
+            <Float>
+              <Astronaut
+                scale={isMobile && 0.23}
+                position={isMobile && [0, -1.5, 0]}
+              />
+              <OrbitControls />
+              {/*{enableZoom={false}*/}
+            </Float>
+          </Suspense>
+          <Rig />
         </Canvas>
       </figure>
     </section>
   );
+
+  function Rig() {
+    return useFrame((state, delta) => {
+      easing.damp3(
+        state.camera.position,
+        [state.mouse.x / 10, 1 + state.mouse.y / 10, 3],
+        0.5,
+        delta
+      );
+    });
+  }
 };
 
 export default Hero;
