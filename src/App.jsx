93d0c4db58a4a98490bdecee7b2d025d7aaac209
Samuel Riedel <samuel.riedel@outlook.com>
2025-07-26 02:31:04 +0000

    created_grids_for_component_about

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import LastCommit from "./components/LastCommit";
+import About from "./sections/About";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <LastCommit />
       <Navbar />
       <Hero />
+      <About />
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
